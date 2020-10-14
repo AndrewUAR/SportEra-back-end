6e7c81340fa0b62a8d91af92c415130f20541d2c
Andrew <andrewriznykua93@gmail.com>
2020-10-14 16:08:02 -0500

    activity repository created

--- src/main/java/com/sportera/sportera/repositories/ActivityRepository.java
+++ src/main/java/com/sportera/sportera/repositories/ActivityRepository.java
@@ -1,2 +1,13 @@
-package com.sportera.sportera.repositories;public interface ActivityRepository {
+package com.sportera.sportera.repositories;
+
+import com.sportera.sportera.models.Activity;
+import com.sportera.sportera.models.EActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    Optional<Activity> findByName(EActivity name);
 }
