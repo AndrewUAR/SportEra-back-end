4818d5d02fc482ff60dc7763c1e4330e4b7037d3
Andrew <andrewriznykua93@gmail.com>
2020-10-14 16:11:03 -0500

    Difficulty model created

--- src/main/java/com/sportera/sportera/models/Difficulty.java
+++ src/main/java/com/sportera/sportera/models/Difficulty.java
@@ -1,2 +1,20 @@
-package com.sportera.sportera.models;public class Difficulty {
+package com.sportera.sportera.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "difficulties")
+public class Difficulty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
 }
