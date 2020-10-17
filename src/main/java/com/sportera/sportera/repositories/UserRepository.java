66fd6c18d69ee80b532b96fbdfb1151f274e1bfa
Andrew <andrewriznykua93@gmail.com>
2020-10-16 23:34:49 -0500

    type changed to Optional

--- src/main/java/com/sportera/sportera/repositories/UserRepository.java
+++ src/main/java/com/sportera/sportera/repositories/UserRepository.java
@@ -4,10 +4,12 @@ import com.sportera.sportera.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmailIgnoreCase(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmailIgnoreCase(String email);
 
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
