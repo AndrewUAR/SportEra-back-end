fc00526b8edd573ff493dccb2c146478edc234c5
Andrew <andrewriznykua93@gmail.com>
2020-10-16 23:34:12 -0500

    type changed to Optional

--- src/main/java/com/sportera/sportera/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/sportera/sportera/security/jwt/AuthTokenFilter.java
@@ -8,11 +8,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -49,11 +50,16 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     }
 
     private String parseJwt(HttpServletRequest request) {
-        String headerAuth = request.getHeader("Authorization");
 
-        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-            return headerAuth.substring(7, headerAuth.length());
-        }
+          Cookie jwt = WebUtils.getCookie(request, "jwt");
+          if (jwt != null) {
+              return jwt.getValue();
+          }
+//        String headerAuth = request.getHeader("Authorization");
+//
+//        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+//            return headerAuth.substring(7, headerAuth.length());
+//        }
 
         return null;
     }
