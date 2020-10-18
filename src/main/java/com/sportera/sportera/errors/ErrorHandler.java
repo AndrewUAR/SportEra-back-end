226c7bd92006a58bbf038466585de9d5e14243bf
Andrew <andrewriznykua93@gmail.com>
2020-10-18 15:08:01 -0500

    exceptionHandler for JWT created

--- src/main/java/com/sportera/sportera/errors/ErrorHandler.java
+++ src/main/java/com/sportera/sportera/errors/ErrorHandler.java
@@ -1,20 +1,90 @@
 package com.sportera.sportera.errors;
 
+import com.sportera.sportera.payloads.response.HttpResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.nio.file.AccessDeniedException;
+import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 public class ErrorHandler implements ErrorController {
+
+    private static final String INCORRECT_CREDENTIALS = "Invalid username or password. Please try again";
+    private static final String VALIDATION_ERROR = "Validation error";
+    private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission to perform this action";
+    private static final String ACCOUNT_LOCKED = "Your account has been locked for 15 minutes";
 
     @Autowired
     private ErrorAttributes errorAttributes;
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<HttpResponse> badCredentialsException() {
+        return createHttpResponse(HttpStatus.UNAUTHORIZED, INCORRECT_CREDENTIALS);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<HttpResponse> accessDeniedException() {
+        return createHttpResponse(HttpStatus.FORBIDDEN, NOT_ENOUGH_PERMISSION);
+    }
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> lockedException() {
+        return createHttpResponse(HttpStatus.UNAUTHORIZED, ACCOUNT_LOCKED);
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<HttpResponse> invalidJwtTokenException(MalformedJwtException exception) {
+        return createHttpResponse(HttpStatus.UNAUTHORIZED, exception.getMessage());
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<HttpResponse> expiredJwtTokenException(ExpiredJwtException exception) {
+        return createHttpResponse(HttpStatus.UNAUTHORIZED, exception.getMessage());
+    }
+
+    @ExceptionHandler(UnsupportedJwtException.class)
+    public ResponseEntity<HttpResponse> unsupportedJwtTokenException(UnsupportedJwtException exception) {
+        return createHttpResponse(HttpStatus.UNAUTHORIZED, exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<HttpResponse> illegalArgumentException(IllegalArgumentException exception) {
+        return createHttpResponse(HttpStatus.UNAUTHORIZED, exception.getMessage());
+    }
+
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
+        return new ResponseEntity<HttpResponse>(new HttpResponse(httpStatus.value(), message), httpStatus);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<HttpResponse> handleValidationException(MethodArgumentNotValidException exception) {
+        HttpResponse response = new HttpResponse(HttpStatus.BAD_REQUEST.value(), VALIDATION_ERROR);
+        BindingResult result = exception.getBindingResult();
+        Map<String, String> validationErrors = new HashMap<>();
+        for(FieldError fieldError: result.getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        response.setValidationErrors(validationErrors);
+        return new ResponseEntity<HttpResponse>(response, HttpStatus.BAD_REQUEST);
+    }
 
     @RequestMapping("error")
     ApiError handleError(WebRequest webRequest) {
