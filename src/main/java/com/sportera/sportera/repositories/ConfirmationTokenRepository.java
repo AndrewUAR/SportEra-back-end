de8054f92b72362dfd2c87c64870073f6a4ed8e3
Andrew <andrewriznykua93@gmail.com>
2020-10-17 23:36:57 -0500

    ConfirmationToken service created

--- src/main/java/com/sportera/sportera/repositories/ConfirmationTokenRepository.java
+++ src/main/java/com/sportera/sportera/repositories/ConfirmationTokenRepository.java
@@ -5,8 +5,10 @@ import com.sportera.sportera.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfirmationTokenRepository  extends JpaRepository<ConfirmationToken, Long> {
-    ConfirmationToken findByConfirmationToken(String confirmationToken);
-    ConfirmationToken findByUser(User user);
+    Optional<ConfirmationToken> findByConfirmationToken(String confirmationToken);
+    Optional<ConfirmationToken> findByUser(User user);
 }
