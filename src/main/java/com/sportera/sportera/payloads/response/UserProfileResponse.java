67ced561930a01c0fc4e0ac6b5401d021c4c3168
Andrew <andrewriznykua93@gmail.com>
2020-10-17 23:40:30 -0500

    userProfile http response class created

--- src/main/java/com/sportera/sportera/payloads/response/UserProfileResponse.java
+++ src/main/java/com/sportera/sportera/payloads/response/UserProfileResponse.java
@@ -1,2 +1,25 @@
-package com.sportera.sportera.payloads.response;public class UserProfileResponse {
+package com.sportera.sportera.payloads.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserProfileResponse {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String introduction;
+    private String profilePicture;
+    private List<String> activities;
+
+    public UserProfileResponse(String firstName, String lastName, int age, String introduction, String profilePicture, List<String> activities) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.introduction = introduction;
+        this.profilePicture = profilePicture;
+        this.activities = activities;
+    }
 }
